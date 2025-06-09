d59c0d08f55723046483664ffd20832bb496d434
Pandeyabhay3007 <pandeyabhay3007@gmail.com>
2025-06-09 16:35:57 +0530

    Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -65,7 +65,7 @@ const Hero = ({ scrollToSection }: HeroProps) => {
                 </h1>
                 
                 {/* Decorative underline */}
-                <div className="w-46.9 h-1 bg-gradient-to-r from-transparent via-white/60 to-transparent lg:mx-0 mx-auto rounded-full"></div>
+                <div className="w-43.9 h-1 bg-gradient-to-r from-transparent via-white/60 to-transparent lg:mx-0 mx-auto rounded-full"></div>
               </div>
               
               {/* Enhanced Role Description */}
